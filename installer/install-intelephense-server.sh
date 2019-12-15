0bd056ae3db454f076edd278237cdb1cca503d51
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-16 02:47:35 +0900

    Add installer

---
+++ installer/install-intelephense-server.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)
+[ -d ../servers/intelephense-server ] && rm -rf ../servers/intelephense-server
+mkdir ../servers/intelephense-server
+cd ../servers/intelephense-server
+git clone "https://github.com/bmewburn/vscode-intelephense" .
+npm install
+npm run compile
+
+cat <<EOF > intelephense-server
+#!/bin/sh
+
+DIR=\$(cd \$(dirname \$0); pwd)
+node \$DIR/node_modules/.bin/intelephense --stdio
+EOF
+
+chmod +x vim-language-server
