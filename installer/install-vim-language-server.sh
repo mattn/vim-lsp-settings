20ea139d60f4cac4841f94119f965979405f4811
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-16 02:38:24 +0900

    Install to local directory

--- installer/install-vim-language-server.sh
+++ installer/install-vim-language-server.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-yarn global add vim-language-server
+set -e
+
+cd $(dirname $0)
+[ -d ../servers/vim-language-server ] && rm -rf ../servers/vim-language-server
+mkdir ../servers/vim-language-server
+cd ../servers/vim-language-server
+git clone https://github.com/iamcco/vim-language-server .
+yarn
+yarn build
+
+cat <<EOF > vim-language-server
+#!/bin/sh
+
+DIR=\$(cd \$(dirname \$0); pwd)
+node bin/index.js --stdio
+EOF
+
+chmod +x vim-language-server
