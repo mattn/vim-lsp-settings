f2a477a389b94e8cf83a53dfde03942b53c9ad50
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-16 01:28:12 +0900

    Add installer for kotlin-language-server

---
+++ installer/install-kotlin-language-server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd $(dirname $0)
+[ -d ../servers/kotlin-language-server ] && rm -rf ../servers/kotlin-language-server
+mkdir ../servers/kotlin-language-server
+cd ../servers/kotlin-language-server
+curl -L -o server.zip 'https://github.com/fwcd/kotlin-language-server/releases/download/0.5.2/server.zip'
+unzip server.zip
+rm server.zip
+
+ln -s server/bin/kotlin-language-server .
