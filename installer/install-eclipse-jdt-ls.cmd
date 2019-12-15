64ae4c65693839224b78a0ceccad792d9b9308a8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-16 01:12:12 +0900

    Fix installer

--- installer/install-eclipse-jdt-ls.cmd
+++ installer/install-eclipse-jdt-ls.cmd
@@ -13,12 +13,13 @@ echo @echo off ^
 
 setlocal enabledelayedexpansion ^
 
-for %%%%F in (%%~dp0/plugins/org.eclipse.equinox.launcher_*.jar) do ( ^
+for %%%%F in (%%~dp0\plugins\org.eclipse.equinox.launcher_*.jar) do ( ^
 
-  set launcher=%%~dp0/plugins/%%%%F ^
+  set launcher=%%%%F ^
 
 ) ^
 
-java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -javaagent:%%~dp0/lombok.jar -Xbootclasspath/a:%%~dp0/lombok.jar -jar %%~dp0\!launcher! -configuration %%~dp0\config_win -data %%~dp0\data ^
+java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -javaagent:%%~dp0/lombok.jar -Xbootclasspath/a:%%~dp0/lombok.jar -jar !launcher! -configuration %%~dp0\config_win -data %%~dp0\data ^
 
 > eclipse-jdt-ls.cmd
+
