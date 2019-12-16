d76744a4ae5612c4d08da082b535d5531183a64f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-16 12:35:17 +0900

    Install gopls local

--- installer/install-gopls.cmd
+++ installer/install-gopls.cmd
@@ -1,3 +1,12 @@
-#!/bin/sh
+@echo off
 
-go get -u golang.org/x/tools/cmd/gopls
+setlocal
+
+cd %~dp0
+if exist "..\servers\gopls" rd /S /Q  "..\servers\gopls"
+md "..\servers\gopls"
+cd "..\servers\gopls"
+set GOPATH=%cd%
+set GOBIN=%cd%
+go get -v -u golang.org/x/tools/cmd/gopls
+rd /S /Q  "src"
--- installer/install-gopls.sh
+++ installer/install-gopls.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-go get -u golang.org/x/tools/cmd/gopls
+set -e
 
+cd $(dirname $0)
+[ -d ../servers/gopls ] && rm -rf ../servers/gopls
+mkdir ../servers/gopls
+cd ../servers/gopls
+go get -u golang.org/x/tools/cmd/gopls
+rm -rf src
