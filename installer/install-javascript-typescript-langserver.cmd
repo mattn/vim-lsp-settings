e5609d52d8cf310bce00e30fb0f9c8fd55685736
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-17 00:02:00 +0900

    Add installer

---
+++ installer/install-javascript-typescript-langserver.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+cd %~dp0
+if exist "..\servers\javascript-typescript-langserver" rd /S /Q  "..\servers\javascript-typescript-langserver"
+md "..\servers\javascript-typescript-langserver"
+cd "..\servers\javascript-typescript-langserver"
+git clone https://github.com/sourcegraph/javascript-typescript-langserver .
+npm install
+npm build
+
+echo @echo off ^
+
+node %%~dp0\lib\language-server-stdio.js ^
+
+> javascript-typescript-langserver.cmd
+
