915dfc3b4e209a255242a929147b19f376626ef6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-17 00:52:46 +0900

    Install loacal

--- installer/install-solargraph.cmd
+++ installer/install-solargraph.cmd
@@ -1,3 +1,15 @@
 @echo off
 
-gem install --user-install solargraph
+cd %~dp0
+if exist "..\servers\solargraph" rd /S /Q  "..\servers\solargraph"
+md "..\servers\solargraph"
+cd "..\servers\solargraph"
+git clone "https://github.com/castwide/solargraph" .
+call bundle install --path vendor/bundle
+
+echo @echo off ^
+
+bundle exec ruby %%~dp0\bin/solargraph stdio ^
+
+> solargraph.cmd
+
--- installer/install-solargraph.sh
+++ installer/install-solargraph.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-gem install --user-install solargraph
+set -e
 
+cd $(dirname $0)
+[ -d ../servers/solargraph ] && rm -rf ../servers/solargraph
+mkdir ../servers/solargraph
+cd ../servers/solargraph
+git clone "https://github.com/castwide/solargraph" .
+call bundle install --path vendor/bundle
+
+cat <<EOF > solargraph
+#!/bin/sh
+
+DIR=\$(cd \$(dirname \$0); pwd)
+bundle exec ruby \$DIR/bin/solargraph stdio
+EOF
+
+chmod +x solargraph
