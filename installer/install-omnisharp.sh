8bd6f2636a357be32eec12eac8da8e673137170d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-19 17:36:24 +0900

    Fix installer

--- installer/install-omnisharp.sh
+++ installer/install-omnisharp.sh
@@ -6,9 +6,9 @@ cd $(dirname $0)
 [ -d ../servers/omnisharp ] && rm -rf ../servers/omnisharp
 mkdir ../servers/omnisharp
 cd ../servers/omnisharp
-curo -L -o omnisharp-linux-x64.zip https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.34.9/omnisharp-linux-x64.zip
+curl -L -o omnisharp-linux-x64.zip https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.34.9/omnisharp-linux-x64.zip
 unzip omnisharp-linux-x64.zip
-del omnisharp-linux-x64.zip
+rm omnisharp-linux-x64.zip
 
 cat <<EOF > omnisharp
 #!/bin/sh
