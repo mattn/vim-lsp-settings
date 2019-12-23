803c9548f0b4c81968139597e56bc4d5b9516c60
ippachi <ivalice1218@gmail.com>
2019-12-23 00:40:21 +0000

    Escape $

--- installer/install-solargraph.sh
+++ installer/install-solargraph.sh
@@ -13,7 +13,7 @@ cat <<EOF > solargraph
 #!/bin/sh
 
 DIR=\$(cd \$(dirname \$0); pwd)
-cd $DIR && bundle exec ruby \$DIR/bin/solargraph stdio
+cd \$DIR && bundle exec ruby \$DIR/bin/solargraph stdio
 EOF
 
 chmod +x solargraph
