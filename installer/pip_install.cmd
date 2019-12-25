f1656ee6787f2459c191f3c42ecb328491c545cd
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-12-26 00:00:37 +0900

    Windows native python3(python.exe) and MSYS2 python3 support

--- installer/pip_install.cmd
+++ installer/pip_install.cmd
@@ -8,6 +8,30 @@ rd /Q /S "%server_dir%" 2>NUL
 md "%server_dir%"
 cd "%server_dir%"
 
+where python  && goto :python
+:python_fail
+where python3 && goto :python3
+:python3_fail
+goto :EOF
+
+:python
+python -c "import sys; from distutils.version import LooseVersion;sys.exit(0 if (LooseVersion(sys.version) > LooseVersion('3')) else 1)"
+if errorlevel 1 goto :python_fail
+
+python -m venv ./venv
+venv\Scripts\pip3 install %2
+
+echo @echo off ^
+
+%%~dp0\venv\Scripts\%1.exe %%* ^
+
+> %1.cmd
+goto :EOF
+
+:python3
+python3 -c "import sys; from distutils.version import LooseVersion;sys.exit(0 if (LooseVersion(sys.version) > LooseVersion('3')) else 1)"
+if errorlevel 1 goto :python3_fail
+
 python3 -m venv ./venv
 venv\bin\pip3 install %2
 
@@ -16,3 +40,4 @@ echo @echo off ^
 %%~dp0\venv\bin\%1.cmd %%* ^
 
 > %1.cmd
+goto :EOF
