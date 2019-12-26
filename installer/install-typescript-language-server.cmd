23b4bb48be8ffa632992e901b3f8d3235f0c7f07
wordi <wordijp@gmail.com>
2019-12-27 02:29:15 +0900

    Use installed tsserver from typescript-language-server

--- installer/install-typescript-language-server.cmd
+++ installer/install-typescript-language-server.cmd
@@ -2,4 +2,5 @@
 
 cd /d %~dp0
 
+call npm_install tsserver typescript
 call npm_install typescript-language-server typescript-language-server
--- installer/install-typescript-language-server.sh
+++ installer/install-typescript-language-server.sh
@@ -5,4 +5,5 @@ set -e
 cd $(dirname $0)
 
 . ./npm.sh
+npm_install tsserver typescript
 npm_install typescript-language-server typescript-language-server
