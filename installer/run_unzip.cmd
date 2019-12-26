c74222458d05b4195a580bafb32df7b7bb727113
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-26 15:42:20 +0900

    Download unzip.exe into installer directory

--- installer/run_unzip.cmd
+++ installer/run_unzip.cmd
@@ -4,6 +4,6 @@ if "x%1" equ "x" goto :EOF
 
 where unzip 2>NUL
 if %ERRORLEVEL% neq 0 (
-    curl -LO https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/unzip.exe
+    curl -L -o %~dp0\unzip.exe https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/unzip.exe
 )
-unzip "%1"
+%~dp0\unzip "%1"
