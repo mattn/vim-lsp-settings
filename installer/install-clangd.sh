d88dafc773e15c0e827f1877c62eed00526671a4
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-12-28 22:30:35 +0100

    Add installer for clangd (Linux)

---
+++ installer/install-clangd.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)
+
+server_dir="../servers/clangd"
+[ -d $server_dir ] && rm -rf $server_dir
+mkdir $server_dir && cd $server_dir
+
+os=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+case $os in
+    linux)
+        platform="pc-linux-gnu"
+        ;;
+    darwin)
+        platform="darwin-apple"
+        ;;
+esac
+
+# Check Ubuntu version
+ubuntu_version=$(lsb_release -a 2>&1 | grep 'Release' | awk '{print $2}')
+
+case $ubuntu_version in
+    14.04|16.04|18.04)
+        platform="linux-gnu-ubuntu-$ubuntu_version"
+        ;;
+esac
+
+filename="clang+llvm-9.0.0-x86_64-$platform"
+url="http://releases.llvm.org/9.0.0/$filename.tar.xz"
+echo "Downloading clangd and LLVM..."
+curl -LO "$url"
+echo "Extracting archive..."
+tar xvf $filename.tar.xz --strip-components=2 $filename/bin/clangd
+rm -fr clang+llvm-9.0.0*
+./clangd --version
