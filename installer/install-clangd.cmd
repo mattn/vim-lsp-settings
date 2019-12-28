6171be8b865c33463786fc3b14c3b11ae84c4b23
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-12-28 23:38:24 +0100

    Update Windows installation

--- installer/install-clangd.cmd
+++ installer/install-clangd.cmd
@@ -13,6 +13,11 @@ cd /d "%server_dir%"
 echo Downloading clang and LLVM...
 curl -L -o LLVM-9.0.0-win64.exe "http://releases.llvm.org/9.0.0/LLVM-9.0.0-win64.exe"
 echo Running setup...
-echo Make sure to add clangd to your PATH
-LLVM-9.0.0-win64.exe
+
+REM Run setup as regular user to avoid a UAC popup
+set __COMPAT_LAYER=RUNASINVOKER
+md tmp
+LLVM-9.0.0-win64.exe /S /D=%cd%\tmp
+copy tmp\bin\clangd.exe clangd.exe
 del LLVM-9.0.0-win64.exe
+rd /Q /S %cd%\tmp
