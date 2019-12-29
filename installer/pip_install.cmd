12efb5a18c22d2e179a2b3561c1f52884fdf97dc
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-12-29 13:39:15 +0900

    server path fix

--- installer/pip_install.cmd
+++ installer/pip_install.cmd
@@ -3,7 +3,7 @@
 if "x%1" equ "x" goto :EOF
 if "x%2" equ "x" goto :EOF
 
-set server_dir="..\servers\%1"
+set server_dir=..\servers\%1
 rd /Q /S "%server_dir%" 2>NUL
 md "%server_dir%"
 cd "%server_dir%"
