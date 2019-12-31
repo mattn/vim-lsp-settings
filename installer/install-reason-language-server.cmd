cd8285037e8397d9a37ad8af8be81c9f8a0d4d94
Prabir <mail@prabir.me>
2019-12-31 13:53:43 -0800

    update reason-language-server to v1.7.5

--- installer/install-reason-language-server.cmd
+++ installer/install-reason-language-server.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-curl -L -o rls-windows.zip "https://github.com/jaredly/reason-language-server/releases/download/1.7.4/rls-windows.zip"
+curl -L -o rls-windows.zip "https://github.com/jaredly/reason-language-server/releases/download/1.7.5/rls-windows.zip"
 call "%~dp0\run_unzip.cmd" rls-windows.zip
 del rls-windows.zip
 
