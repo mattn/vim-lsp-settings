c8d7356cd809fffee36d9e3542d85b5b08b9bbae
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-02 20:57:36 +0900

    Add dart language server

---
+++ installer/install-analysis-server-dart-snapshot.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+curl -o "dartsdk-windows-x64-release.zip" "https://storage.googleapis.com/dart-archive/channels/dev/release/latest/sdk/dartsdk-windows-x64-release.zip"
+call run_unzip.cmd "dartsdk-windows-x64-release.zip"
+del "dartsdk-windows-x64-release.zip"
+
+echo @echo off ^
+
+%%~dp0\bin\dart %%~dp0\snapshots\analysis_server.dart.snapshot --lsp %%* ^
+
+> analysis-server-dart-snapshot.cmd
