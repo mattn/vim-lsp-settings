5cbb60f9c6a0061250acb75c37bb3b0b75c2081f
tyru <tyru.exe@gmail.com>
2020-01-09 11:35:09 +0900

    fix: clangd: remove unnecessary mkdir & cd

--- installer/install-clangd.cmd
+++ installer/install-clangd.cmd
@@ -2,14 +2,6 @@
 
 setlocal
 
-cd /d %~dp0
-
-set installer_dir=%cd%
-set server_dir=..\servers\clangd
-if exist %server_dir% rd /Q /S "%server_dir%"
-md "%server_dir%"
-cd /d "%server_dir%"
-
 echo Downloading clang and LLVM...
 curl -L -o LLVM-9.0.0-win64.exe "http://releases.llvm.org/9.0.0/LLVM-9.0.0-win64.exe"
 echo Running setup...
