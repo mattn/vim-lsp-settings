f033984b22e73c93169a6d9cea244d7b623fb20a
tyru <tyru.exe@gmail.com>
2020-01-14 16:45:58 +0900

    bump metals version: 0.7.6 -> 0.8.0

--- installer/install-metals.sh
+++ installer/install-metals.sh
@@ -5,5 +5,5 @@ set -e
 curl -Lo ./coursier https://git.io/coursier-cli
 chmod +x ./coursier
 
-version="0.7.6"
+version="0.8.0"
 java -jar ./coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:$version" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o ./metals
