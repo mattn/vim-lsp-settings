8a75f94bbbf2e2a0574883a61f0ef04bc5855c33
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-30 19:07:31 +0900

    Fix configDir of eclipse-jdt-ls
    Fixes #123

--- installer/install-eclipse-jdt-ls.sh
+++ installer/install-eclipse-jdt-ls.sh
@@ -7,12 +7,19 @@ curl -o lombok.jar 'https://projectlombok.org/downloads/lombok.jar'
 tar xvf jdt-language-server-latest.tar.gz
 rm jdt-language-server-latest.tar.gz
 
+osType="\$(uname -s)"
+case "\${osType}" in
+    Darwin*)    configDir=config_mac;;
+    Linux*)     configDir=config_linux;;
+    *)          configDir=config_linux
+esac
+
 cat <<EOF > eclipse-jdt-ls
 #!/usr/bin/env bash
 
 DIR=\$(cd \$(dirname \$0); pwd)
 LAUNCHER=\$(ls \$DIR/plugins/org.eclipse.equinox.launcher_*.jar)
-java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -javaagent:\$DIR/lombok.jar -Xbootclasspath/a:\$DIR/lombok.jar -jar \$LAUNCHER -configuration \$DIR/config_linux -data \$DIR/data
+java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -javaagent:\$DIR/lombok.jar -Xbootclasspath/a:\$DIR/lombok.jar -jar \$LAUNCHER -configuration \$DIR/$configDir -data \$DIR/data
 EOF
 
 chmod +x eclipse-jdt-ls
