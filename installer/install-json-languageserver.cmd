ca187115385a8cf1c5d1aa11c2be05853646c70e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-03-06 01:00:22 +0900

    Install json-languageserver from git repository

--- installer/install-json-languageserver.cmd
+++ installer/install-json-languageserver.cmd
@@ -1,3 +1,50 @@
 @echo off
 
-call "%~dp0\npm_install.cmd" json-languageserver vscode-json-languageserver-bin
+git clone --depth=1 https://github.com/vscode-langservers/vscode-json-languageserver ./
+
+echo {^
+
+	"compilerOptions": {^
+
+		"target": "es2018",^
+
+		"module": "commonjs",^
+
+		"strict": true,^
+
+		"alwaysStrict": true,^
+
+		"noImplicitAny": true,^
+
+		"noImplicitReturns": true,^
+
+		"noUnusedLocals": true,^
+
+		"noUnusedParameters": true,^
+
+		"outDir": "./out",^
+
+		"sourceMap": true,^
+
+		"sourceRoot": "../src"^
+
+	},^
+
+	"include": [^
+
+		"src/**/*"^
+
+	]^
+
+}^
+
+> tsconfig.json
+
+call npm i
+call npx tsc -p .
+
+echo @echo off ^
+
+node %%~dp0\out\jsonServerMain.js %%* ^
+
+> json-languageserver.cmd
