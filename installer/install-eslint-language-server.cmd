a0372cc838388515ab8a14dc30856ead9296f3f0
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-03-06 15:24:43 +0900

    Fix installer of vls

--- installer/install-eslint-language-server.cmd
+++ installer/install-eslint-language-server.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-curl -L -o "vscode-eslint.vsix" "https://github.com/microsoft/vscode-eslint/releases/download/release%2F2.1.0-next.1/vscode-eslint-2.1.0.vsix"
+curl -L -o "vscode-eslint.vsix" "https://github.com/microsoft/vscode-eslint/releases/download/release%%2F2.1.0-next.1/vscode-eslint-2.1.0.vsix"
 call "%~dp0\run_unzip.cmd" vscode-eslint.vsix
 del vscode-eslint.vsix
 
