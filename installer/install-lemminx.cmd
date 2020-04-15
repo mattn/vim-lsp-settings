6900657a494ab66f3645d107fecffce66bfcf5f8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-04-15 23:19:21 +0900

    Fix install-lemminx.cmd

--- installer/install-lemminx.cmd
+++ installer/install-lemminx.cmd
@@ -2,9 +2,22 @@
 
 curl -LO "https://repo.eclipse.org/content/repositories/lemminx-snapshots/org/eclipse/org.eclipse.lemminx/0.11.0-SNAPSHOT/org.eclipse.lemminx-0.11.0-20200309.180941-1-uber.jar"
 
+setlocal
+set VSCODE_XML_VERSION=0.11.0
+set LEMMINX_VERSION=0.11.1
+set URL=https://github.com/redhat-developer/vscode-xml/releases/download/%VSCODE_XML_VERSION%/redhat.vscode-xml-%VSCODE_XML_VERSION%.vsix
+set EXTENTION=redhat.vscode-xml-%VSCODE_XML_VERSION%.vsix
+set LEMMINX_JAR=org.eclipse.lemminx-%LEMMINX_VERSION%-uber.jar
+
+curl -L %URL% -o %EXTENTION%
+call "%~dp0\run_unzip.cmd" -d vscode-xml %EXTENTION%
+
+del %EXTENTION%
+copy vscode-xml\extension\server\%LEMMINX_JAR% .
+
 echo @echo off ^
 
-java -jar %%~dp0\org.eclipse.lemminx-0.11.0-20200309.180941-1-uber.jar ^
+java -jar %%~dp0\%LEMMINX_JAR% %%* ^
 
 > lemminx.cmd
 
--- installer/run_unzip.cmd
+++ installer/run_unzip.cmd
@@ -4,8 +4,8 @@ if "x%1" equ "x" goto :EOF
 
 where unzip 2>NUL
 if %ERRORLEVEL% equ 0 (
-    unzip "%1"
+    unzip %*
 ) else (
     curl -L -o %~dp0\unzip.exe https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/unzip.exe
-    %~dp0\unzip "%1"
+    %~dp0\unzip %*
 )
