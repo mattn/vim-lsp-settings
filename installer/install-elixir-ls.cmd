a3ce21f911b65c1b23fe04c862660385d8a6c2c6
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-04-16 11:39:59 +0700

    Upgrade elixir-ls
    Now elixir-ls move to new repository [1]. So this changes change the
    download path and bump to a new version (v0.3.3).
    
    [1] https://github.com/elixir-lsp/elixir-ls

--- installer/install-elixir-ls.cmd
+++ installer/install-elixir-ls.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-curl -L -o elixir-ls.zip "https://github.com/JakeBecker/elixir-ls/releases/download/v0.2.25/elixir-ls.zip"
+curl -L -o elixir-ls.zip "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.3.3/elixir-ls.zip"
 call "%~dp0\run_unzip.cmd" elixir-ls.zip
 del elixir-ls.zip
 
