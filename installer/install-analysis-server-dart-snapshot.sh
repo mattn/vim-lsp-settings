cceb39876ef37127801a964da2b97b8be8ce8f4e
tksgi <23550705+tksgi@users.noreply.github.com>
2020-05-11 02:31:29 +0900

    add MacOS installer for dart analysis server

--- installer/install-analysis-server-dart-snapshot.sh
+++ installer/install-analysis-server-dart-snapshot.sh
@@ -2,14 +2,27 @@
 
 set -e
 
-curl -o "dartsdk-linux-x64-release.zip" "https://storage.googleapis.com/dart-archive/channels/dev/release/latest/sdk/dartsdk-linux-x64-release.zip"
-unzip "dartsdk-linux-x64-release.zip"
-rm "dartsdk-linux-x64-release.zip"
+os=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+case $os in
+linux)
+  platform="linux"
+  ;;
+darwin)
+  platform="macos"
+  ;;
+esac
+
+filename="dartsdk-$platform-x64-release.zip"
+
+curl -o "$filename" "https://storage.googleapis.com/dart-archive/channels/dev/release/latest/sdk/dartsdk-$platform-x64-release.zip"
+unzip "$filename"
+rm "$filename"
 
 cat <<EOF >analysis-server-dart-snapshot
 #!/usr/bin/env bash
 
-DIR=\$(cd \$(dirname \$0); pwd)
+DIR=\$(cd \$(dirname \$0); pwd)/dart-sdk
 \$DIR/bin/dart \$DIR/bin/snapshots/analysis_server.dart.snapshot --lsp \$*
 EOF
 
