6e164a3153ca26581c37366e6b5f0a9a9606a938
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-06-03 20:15:40 +0900

    Fix quote

--- installer/install-clojure-lsp.sh
+++ installer/install-clojure-lsp.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-version="20191202T142318""
+version="20191202T142318"
 curl -L -o clojure-lsp https://github.com/snoe/clojure-lsp/releases/download/release-$version/clojure-lsp
 chmod +x clojure-lsp
