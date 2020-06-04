6d2e981189f35d689d17876c086bb6fa6bb1b144
Bastien Laine <bastien.laine@gmail.com>
2020-06-04 13:15:34 +0800

    Update installer/install-clj-kondo-lsp.cmd
    Co-authored-by: mattn <mattn.jp@gmail.com>

--- installer/install-clj-kondo-lsp.cmd
+++ installer/install-clj-kondo-lsp.cmd
@@ -1,3 +1,4 @@
 @echo off
-New-Variable -Name "VERSION" -Value "2020.05.09"
+setlocal
+set VERSION=2020.05.09
 curl -L -o clj-kondo-lsp https://github.com/borkdude/clj-kondo/releases/download/v$VERSION/clj-kondo-lsp-server-$VerSION-standalone.jar
