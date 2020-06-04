1941db037087db5ef2702a37beb99193a3815daf
Bastien Laine <me@bastien-laine.io>
2020-06-04 13:01:00 +0800

    [edit] Fix typo + add $VERSION

--- installer/install-clj-kondo-lsp.cmd
+++ installer/install-clj-kondo-lsp.cmd
@@ -1,3 +1,3 @@
 @echo off
-
-curl -L -o clj-kondo-lsp https://github.com/borkdude/clj-kondo/releases/download/v2020.05.09/clj-kondo-lsp-server-2020.05.09-standalone.jar
+New-Variable -Name "VERSION" -Value "2020.05.09"
+curl -L -o clj-kondo-lsp https://github.com/borkdude/clj-kondo/releases/download/v$VERSION/clj-kondo-lsp-server-$VerSION-standalone.jar
--- installer/install-clj-kondo-lsp.sh
+++ installer/install-clj-kondo-lsp.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-curl -L -o clj-kondo-lsp https://github.com/borkdude/clj-kondo/releases/download/v2020.05.09/clj-kondo-lsp-server-2020.05.09-standalone.jar
+VERSION="2020.05.09"
+curl -L -o clj-kondo-lsp https://github.com/borkdude/clj-kondo/releases/download/v$VERSION/clj-kondo-lsp-server-$VERSION-standalone.jar
