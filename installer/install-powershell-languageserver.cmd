750cbb0865f1465d0981f949b5833e7c8c274d58
sgur <sgurrr@gmail.com>
2020-06-08 16:59:27 +0900

    Trim trailing whitespaces to fix invalid environment variable values

--- installer/install-powershell-languageserver.cmd
+++ installer/install-powershell-languageserver.cmd
@@ -7,14 +7,14 @@ call "%~dp0\run_unzip.cmd" PowerShellEditorServices.zip
 del PowerShellEditorServices.zip
 mkdir %~dp0session
 
-echo @echo off ^
+echo @echo off^
 
-setlocal ^
+setlocal^
 
-set PSES_BUNDLE_PATH=%%~dp0PowerShellEditorServices ^
+set PSES_BUNDLE_PATH=%%~dp0PowerShellEditorServices^
 
-set SESSION_TEMP_PATH=%%~dp0session ^
+set SESSION_TEMP_PATH=%%~dp0session^
 
-powershell -NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "%%PSES_BUNDLE_PATH:\=/%%/PowerShellEditorServices/Start-EditorServices.ps1 -BundledModulesPath %%PSES_BUNDLE_PATH:\=/%% -LogPath %%SESSION_TEMP_PATH:\=/%%/logs.log -SessionDetailsPath %%SESSION_TEMP_PATH:\=/%%/session.json -FeatureFlags @() -AdditionalModules @() -HostName 'My Client' -HostProfileId 'myclient' -HostVersion 1.0.0 -Stdio -LogLevel Normal" ^
+powershell -NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "%%PSES_BUNDLE_PATH:\=/%%/PowerShellEditorServices/Start-EditorServices.ps1 -BundledModulesPath %%PSES_BUNDLE_PATH:\=/%% -LogPath %%SESSION_TEMP_PATH:\=/%%/logs.log -SessionDetailsPath %%SESSION_TEMP_PATH:\=/%%/session.json -FeatureFlags @() -AdditionalModules @() -HostName 'My Client' -HostProfileId 'myclient' -HostVersion 1.0.0 -Stdio -LogLevel Normal"^
 
 > powershell-languageserver.cmd
