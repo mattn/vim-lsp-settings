730eb9caa8e141284aae71a8878d9b44c435c126
Milly <milly.ca@gmail.com>
2020-06-19 17:30:32 +0900

    Show running commands.

--- installer/pip_install.cmd
+++ installer/pip_install.cmd
@@ -1,11 +1,16 @@
 @echo off
 
+REM Usage
+REM : pip_install [EXECUTABLE_NAME] [PYPI_NAME]
+
 if "x%1" equ "x" goto :EOF
 if "x%2" equ "x" goto :EOF
 
 REM python(ver 3.x) or python3 check
+echo :where python
 where python  2>NUL && goto :python
 :python_fail
+echo :where python3
 where python3 2>NUL && goto :python3
 :python3_fail
 goto :EOF
@@ -26,8 +31,10 @@ goto :create_venv
 
 :create_venv
 REM python support slash path split?
+echo :call "%PYTHON%" -m venv ./venv
 call "%PYTHON%" -m venv ./venv
 if not errorlevel 1 goto :install
+echo :call "%PYTHON%" -m venv .\venv
 call "%PYTHON%" -m venv .\venv
 goto :install
 
@@ -45,12 +52,16 @@ for /f %%V in ("%PYTHON%") do set BINPATH=%%~dpV
 
 REM upgrade pip (windows: use `python -m pip`)
 REM and install target package
+echo :"%PYTHON%" -m pip install -U pip
 "%PYTHON%" -m pip install -U pip
+echo :"%PYTHON%" -m pip install %2
 "%PYTHON%" -m pip install %2
 
 REM exec cmd generate
+echo :generate %1.cmd
 echo @echo off ^
 
 "%BINPATH%%1" %%* ^
 
 > %1.cmd
+type %1.cmd
