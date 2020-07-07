5f702cef449bb77869f041cb0757c913803d018e
dalance <dalance@gmail.com>
2020-07-07 14:17:16 +0900

    Fix svls download path (#272)

--- installer/install-svls.sh
+++ installer/install-svls.sh
@@ -18,6 +18,6 @@ darwin)
 esac
 
 version="v0.1.12"
-curl -L -o svls-$version-x86_64-$os.zip "https://github.com/dalance/svls/releases/download/v0.1.12/svls-$version-x86_64-$os.zip"
+curl -L -o svls-$version-x86_64-$os.zip "https://github.com/dalance/svls/releases/download/$version/svls-$version-x86_64-$os.zip"
 unzip svls-$version-x86_64-$os.zip
 rm svls-$version-x86_64-$os.zip
