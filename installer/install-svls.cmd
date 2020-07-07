df1659d5df060e0b88e04891fb2c90515feaeb01
dalance <dalance@gmail.com>
2020-07-07 23:35:18 +0900

    Update svls to v0.1.16 (#273)

--- installer/install-svls.cmd
+++ installer/install-svls.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=v0.1.12
+set VERSION=v0.1.16
 curl -L -o svls-%VERSION%-x86_64-win.zip "https://github.com/dalance/svls/releases/download/%VERSION%/svls-%VERSION%-x86_64-win.zip"
 call "%~dp0\run_unzip.cmd" svls-%VERSION%-x86_64-win.zip
 move target\x86_64-pc-windows-msvc\release\svls.exe .
