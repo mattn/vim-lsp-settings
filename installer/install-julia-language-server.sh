b262913e6c561db9dddc70930d8168ef581674d2
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-07-15 23:02:00 +0900

    fix julia language server for *nix

--- installer/install-julia-language-server.sh
+++ installer/install-julia-language-server.sh
@@ -2,25 +2,13 @@
 
 set -e
 
-julia -e 'using Pkg; Pkg.add(PackageSpec(name="LanguageServer", rev="master"))'
-julia -e 'using Pkg; Pkg.add(PackageSpec(name="SymbolServer", rev="master"))'
-julia -e 'using Pkg; Pkg.add(PackageSpec(name="CSTParser", rev="master"))'
-
-cat <<EOF >languageserver.jl
-import LanguageServer
-import Pkg
-import SymbolServer
-envpath = dirname(Pkg.Types.Context().env.project_file)
-server = LanguageServer.LanguageServerInstance(stdin, stdout, false, envpath)
-server.runlinter = true
-run(server)
-EOF
+julia -e 'using Pkg; Pkg.add("LanguageServer")'
 
 cat <<EOF >julia-language-server
 #!/usr/bin/env bash
 
 DIR=\$(cd \$(dirname \$0); pwd)
-julia \$DIR/languageserver.jl \$*
+julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()"
 EOF
 
 chmod +x julia-language-server
