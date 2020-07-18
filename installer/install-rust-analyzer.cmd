c5e72e0d730e6f2b6c34a55acfa7a0b3672fa069
Prabir Shrestha <mail@prabir.me>
2020-07-18 17:14:42 +0000

    update rust-analyzer to 2020-07-13

--- installer/install-rust-analyzer.cmd
+++ installer/install-rust-analyzer.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=2020-06-29
+set VERSION=2020-07-13
 curl -L -o rust-analyzer-windows.exe "https://github.com/rust-analyzer/rust-analyzer/releases/download/%VERSION%/rust-analyzer-windows.exe"
 
 move rust-analyzer-windows.exe rust-analyzer.exe
