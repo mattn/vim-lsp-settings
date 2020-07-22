aee96366c532cc26cad2392f3cf680387c3a32b3
Prabir Shrestha <mail@prabir.me>
2020-07-22 02:01:03 -0700

    update rust-analyzer to 2020-07-13 (#285)

--- installer/install-rust-analyzer.sh
+++ installer/install-rust-analyzer.sh
@@ -13,7 +13,7 @@ darwin)
   ;;
 esac
 
-version="2020-06-29"
+version="2020-07-13"
 curl -L -o rust-analyzer-$platform "https://github.com/rust-analyzer/rust-analyzer/releases/download/$version/rust-analyzer-$platform"
 
 mv rust-analyzer-$platform rust-analyzer
