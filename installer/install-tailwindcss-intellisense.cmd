d74b2fe9ca12ecc33a546e75c5f3247578c4c2ef
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2020-07-31 00:41:26 +0900

    Fix oops

--- installer/install-tailwindcss-intellisense.cmd
+++ installer/install-tailwindcss-intellisense.cmd
@@ -2,7 +2,7 @@
 
 setlocal
 set VERSION=0.4.1
-curl -L -o "vscode-lua.vsix" "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%VERSION%/vscode-tailwindcss--%VERSION%.vsix"
+curl -L -o "vscode-tailwindcss.vsix" "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%VERSION%/vscode-tailwindcss--%VERSION%.vsix"
 
 call "%~dp0\run_unzip.cmd" vscode-tailwindcss.vsix
 del vscode-tailwindcss.vsix
