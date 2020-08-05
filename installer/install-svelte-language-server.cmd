a2ae16501b7c30114e2978fb471c5a65fa133084
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-08-06 00:48:40 +0900

    Fix installer for svelte-language-server

--- installer/install-svelte-language-server.cmd
+++ installer/install-svelte-language-server.cmd
@@ -1,15 +1,4 @@
 @echo off
 
-git clone --depth=1 https://github.com/sveltejs/language-tools .
-
-cd packages\language-server
-call npm install
-call npm run build
-cd ..\..
-
-echo @echo off ^
-
-node %%~dp0\packages\language-server\bin\server.js %%* ^
-
-> svelte-language-server.cmd
-
+call "%~dp0\npm_install.cmd" svelteserver svelte-language-server
+ren svelteserver.cmd svelte-language-server.cmd
--- installer/install-svelte-language-server.sh
+++ installer/install-svelte-language-server.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-git clone --depth=1 https://github.com/sveltejs/language-tools .
+"$(dirname "$0")/npm_install.sh" svelteserver svelte-language-server
+mv svelteserver svelte-language-server
 
-(cd packages/language-server && npm install && npm run build)
-
-cat <<EOF >svelte-language-server
-#!/usr/bin/env bash
-
-DIR=\$(cd \$(dirname \$0); pwd)
-node \$DIR/packages/language-server/bin/server.js \$*
-EOF
-
-chmod +x svelte-language-server
