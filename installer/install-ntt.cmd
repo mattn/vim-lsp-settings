753322a2c9fd8c2d3f0b96e40480a672aaab648e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-11-19 00:51:16 +0900

    Fix Windows installer of ntt

--- installer/install-ntt.cmd
+++ installer/install-ntt.cmd
@@ -2,4 +2,4 @@
 
 setlocal
 set VERSION=0.5
-curl -L "https://github.com/nokia/ntt/releases/download/v%VERSION%/ntt_windows_x86_64.tar.gz" | tar xz ntt
+curl -L "https://github.com/nokia/ntt/releases/download/v%VERSION%/ntt_windows_x86_64.tar.gz" | tar xz ntt.exe
