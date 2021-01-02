208428757441d3031dc52e8bfe3b8ea824b44dab
yaegassy <yaegassy@users.noreply.github.com>
2021-01-02 21:59:11 +0900

    Update pyls-ms to 0.5.59 (#364)

--- installer/install-pyls-ms.cmd
+++ installer/install-pyls-ms.cmd
@@ -5,7 +5,7 @@ set VERSION=3.1.1
 curl -L -o dotnet-runtime-%VERSION%-win-x64.zip "https://download.visualstudio.microsoft.com/download/pr/d9768135-4646-4839-9eea-b404bb940452/8275e4320514bab636b1627c62906ef9/dotnet-runtime-%VERSION%-win-x64.zip"
 call "%~dp0\run_unzip.cmd" dotnet-runtime-%VERSION%-win-x64.zip
 
-set VERSION=0.5.51
+set VERSION=0.5.59
 set url=https://pvsc.blob.core.windows.net/python-language-server-stable/Python-Language-Server-win-x64.%VERSION%.nupkg
 
 set nupkg=./pyls.nupkg
