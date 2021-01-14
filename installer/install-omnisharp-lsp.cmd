5fa8bf0b86b07ce83ba5281fde1eca93a1a06c83
Wayne Bowie <wayneb@pcius.com>
2021-01-14 17:17:28 -0600

    Updated Omnisharp to 1.37.5

--- installer/install-omnisharp-lsp.cmd
+++ installer/install-omnisharp-lsp.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=1.35.3
+set VERSION=1.37.5
 curl -L -o omnisharp-win-x64.zip "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v%VERSION%/omnisharp-win-x64.zip"
 call "%~dp0\run_unzip.cmd" omnisharp-win-x64.zip
 del omnisharp-win-x64.zip
