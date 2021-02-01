c20caf91969904362a83746f2b1f42cdea3906e9
yaegassy <yaegassy@users.noreply.github.com>
2021-02-01 11:59:18 +0900

    Changed lemminx download URL and upgraded version (#383)

--- installer/install-lemminx.sh
+++ installer/install-lemminx.sh
@@ -2,20 +2,11 @@
 
 set -e
 
-# Latest version is available on https://repo.eclipse.org/content/repositories/lemminx-snapshots/org/eclipse/org.eclipse.lemminx/.
-# But the lemminx snapshot jar on repo.eclipse.org seems to be obsolete when a new version is released.
-# To avoid confusion, this script refers to lemminx bundled with vscode-xml.
+version="0.14.1"
+url="https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx/${version}/org.eclipse.lemminx-${version}-uber.jar"
+lemminx_jar="org.eclipse.lemminx-${version}-uber.jar"
 
-vscode_xml_version="0.11.0"
-lemminx_version="0.11.1"
-url="https://github.com/redhat-developer/vscode-xml/releases/download/${vscode_xml_version}/redhat.vscode-xml-${vscode_xml_version}.vsix"
-extention="redhat.vscode-xml-${vscode_xml_version}.vsix"
-lemminx_jar="org.eclipse.lemminx-${lemminx_version}-uber.jar"
-
-curl -L "$url" -o "$extention"
-unzip -d vscode-xml "$extention"
-rm "$extention"
-ln -s "vscode-xml/extension/server/${lemminx_jar}" .
+curl -L "$url" -o "${lemminx_jar}"
 
 cat <<EOF >lemminx
 #!/usr/bin/env bash
