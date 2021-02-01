89f8b5ec14d36d79826a7325f31fcfeef8bb5376
yaegassy <yaegassy@users.noreply.github.com>
2021-02-01 11:32:56 +0900

    Changed lemminx download URL and upgraded version

--- installer/install-lemminx.cmd
+++ installer/install-lemminx.cmd
@@ -1,19 +1,11 @@
 @echo off
 
-curl -LO "https://repo.eclipse.org/content/repositories/lemminx-snapshots/org/eclipse/org.eclipse.lemminx/0.11.0-SNAPSHOT/org.eclipse.lemminx-0.11.0-20200309.180941-1-uber.jar"
-
 setlocal
-set VSCODE_XML_VERSION=0.11.0
-set LEMMINX_VERSION=0.11.1
-set URL=https://github.com/redhat-developer/vscode-xml/releases/download/%VSCODE_XML_VERSION%/redhat.vscode-xml-%VSCODE_XML_VERSION%.vsix
-set EXTENTION=redhat.vscode-xml-%VSCODE_XML_VERSION%.vsix
-set LEMMINX_JAR=org.eclipse.lemminx-%LEMMINX_VERSION%-uber.jar
+set VERSION=0.14.1
+set URL=https://repo.eclipse.org/content/repositories/lemminx-releases/org/eclipse/lemminx/org.eclipse.lemminx/%VERSION%/org.eclipse.lemminx-%VERSION%-uber.jar
+set LEMMINX_JAR=org.eclipse.lemminx-%VERSION%-uber.jar
 
-curl -L %URL% -o %EXTENTION%
-call "%~dp0\run_unzip.cmd" -d vscode-xml %EXTENTION%
-
-del %EXTENTION%
-copy vscode-xml\extension\server\%LEMMINX_JAR% .
+curl -L %URL% -o %LEMMINX_JAR%
 
 echo @echo off ^
 
