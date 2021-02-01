b66936a5e88704f4d4b1b189d224e2ea6f161cbb
Marcos Ferreira <mracos@users.noreply.github.com>
2021-02-01 17:51:17 -0300

    Update clojure-lsp repo and version

--- installer/install-clojure-lsp.sh
+++ installer/install-clojure-lsp.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-version="20200706T152722"
-curl -L -o clojure-lsp https://github.com/snoe/clojure-lsp/releases/download/release-$version/clojure-lsp
+version="2021.02.01-20.37.52"
+curl -L -o clojure-lsp "https://github.com/clojure-lsp/clojure-lsp/releases/download/$version/clojure-lsp"
 chmod +x clojure-lsp
