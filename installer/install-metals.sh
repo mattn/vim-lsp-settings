7e81adc3768369bc38bc5c823a1504c628d2696c
Justin Heyes-Jones <justinhj@gmail.com>
2021-02-01 22:20:31 -0800

    Update metals to 0.9.10 (#386)

--- installer/install-metals.sh
+++ installer/install-metals.sh
@@ -5,5 +5,5 @@ set -e
 curl -Lo ./coursier https://git.io/coursier-cli
 chmod +x ./coursier
 
-version="0.9.4"
+version="0.9.10"
 java -jar ./coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:$version" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o ./metals
