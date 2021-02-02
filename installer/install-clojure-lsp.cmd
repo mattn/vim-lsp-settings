de33e9544979f4a707915964bdc09cd87fc67b5d
Marcos Ferreira <mracos@users.noreply.github.com>
2021-02-02 08:22:54 -0300

    Update install-clojure-lsp.cmd

--- installer/install-clojure-lsp.cmd
+++ installer/install-clojure-lsp.cmd
@@ -1,5 +1,5 @@
 @echo off
 
 setlocal
-set VERSION="2021.02.01-20.37.52"
-curl -L -o clojure-lsp.cmd "https://github.com/clojure-lsp/clojure-lsp/releases/download/%VERSION%/clojure-lsp"
+set VERSION=2021.02.01-20.37.52
+curl -L -o clojure-lsp.cmd https://github.com/clojure-lsp/clojure-lsp/releases/download/%VERSION%/clojure-lsp
