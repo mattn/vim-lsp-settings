101d48546463ed7bd8706d8ed3f6199c6919f816
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-02-12 17:18:31 +0900

    Fix install-tailwindcss-intellisense

--- installer/install-tailwindcss-intellisense.cmd
+++ installer/install-tailwindcss-intellisense.cmd
@@ -2,10 +2,10 @@
 
 setlocal
 set VERSION=0.5.9
-curl -L -o "vscode-tailwindcss.vsix" "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%VERSION%/vscode-tailwindcss--%VERSION%.vsix"
+curl -L -o "vscode-tailwindcss.vsix" "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%VERSION%/vscode-tailwindcss-%VERSION%.vsix"
 
 call "%~dp0\run_unzip.cmd" vscode-tailwindcss.vsix
-del vscode-tailwindcss.vsix
+rem del vscode-tailwindcss.vsix
 
 echo @echo off ^
 
