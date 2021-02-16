8f91deb54fb5225c55b063f5af3347c62070299b
Prabir Shrestha <mail@prabir.me>
2021-02-16 00:17:22 +0000

    auto download latest rust-analyzer if in bash

--- installer/install-rust-analyzer.sh
+++ installer/install-rust-analyzer.sh
@@ -13,7 +13,12 @@ darwin)
   ;;
 esac
 
-version="2021-01-18"
+if [ ! which grep &> /dev/null ] || [ ! which cut &> /dev/null ]; then
+  version="2021-02-15"
+else
+  version=$(curl -sL https://api.github.com/repos/rust-analyzer/rust-analyzer/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+fi
+
 curl -L -o rust-analyzer-$platform "https://github.com/rust-analyzer/rust-analyzer/releases/download/$version/rust-analyzer-$platform"
 
 mv rust-analyzer-$platform rust-analyzer
