10ee3172aa1d4cacc604cad0325f26ba80b674cf
mattn <mattn.jp@gmail.com>
2021-02-19 00:29:02 +0900

    Use http_proxy/https_proxy (#396)
    * Use http_proxy/https_proxy
    
    * Fix JAVA_FLAGS

--- installer/install-metals.cmd
+++ installer/install-metals.cmd
@@ -6,4 +6,11 @@ curl -Lo coursier https://git.io/coursier-cli
 curl -Lo coursier.bat https://git.io/coursier-bat
 
 set VERSION=0.9.10
-java %JAVA_OPTS% -jar coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:%VERSION%" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o metals
+set JAVA_FLAGS=
+if "%HTTPS_PROXY%" neq "" (
+  set JAVA_FLAGS=-Dhttps.proxyHost=%HTTPS_PROXY%
+)
+if "%HTTP_PROXY%" neq "" (
+  set JAVA_FLAGS=-Dhttp.proxyHost=%HTTP_PROXY% %JAVA_FLAGS%
+)
+java %JAVA_FLAGS% -jar coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:%VERSION%" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o metals
