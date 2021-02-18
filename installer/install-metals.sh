7fad4a66909e4fe558bd16012ea7f61c46863995
matawed <30142056+matawed@users.noreply.github.com>
2021-02-18 15:56:04 +0100

    Fix: add support for http proxy when pulling with java (#393)
    Co-authored-by: Markus Tauchnitz <markus.tauchnitz@bmw.de>

--- installer/install-metals.sh
+++ installer/install-metals.sh
@@ -6,4 +6,22 @@ curl -Lo ./coursier https://git.io/coursier-cli
 chmod +x ./coursier
 
 version="0.9.10"
-java -jar ./coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:$version" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o ./metals
+java_flags=()
+
+if [[ -n "${https_proxy}" ]]; then
+  readonly https_proxy_without_protocol="${https_proxy#http://}"
+  java_flags+=("-Dhttps.proxyHost=${https_proxy_without_protocol%:*}")
+  java_flags+=("-Dhttps.proxyPort=${https_proxy_without_protocol##*:}")
+fi
+
+if [[ -n "${http_proxy}" ]]; then
+  http_proxy_without_protocol="${http_proxy#http://}"
+  java_flags+=("-Dhttp.proxyHost=${http_proxy_without_protocol%:*}")
+  java_flags+=("-Dhttp.proxyPort=${http_proxy_without_protocol##*:}")
+fi
+
+if [[ -n "${no_proxy}" ]]; then
+  java_flags+=("-Dhttp.nonProxyHosts=${no_proxy}")
+fi
+
+java "${java_flags[@]}" -jar ./coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:${version}" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o ./metals
