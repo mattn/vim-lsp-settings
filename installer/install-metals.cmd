4ba5612b74d7855fa4238ccebdec5cbd72064ad2
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-02-19 00:22:44 +0900

    Fix JAVA_FLAGS

--- installer/install-metals.cmd
+++ installer/install-metals.cmd
@@ -11,6 +11,6 @@ if "%HTTPS_PROXY%" neq "" (
   set JAVA_FLAGS=-Dhttps.proxyHost=%HTTPS_PROXY%
 )
 if "%HTTP_PROXY%" neq "" (
-  set JAVA_FLAGS=-Dhttp.proxyHost=%HTTP_PROXY%
+  set JAVA_FLAGS=-Dhttp.proxyHost=%HTTP_PROXY% %JAVA_FLAGS%
 )
 java %JAVA_FLAGS% -jar coursier bootstrap --ttl Inf "org.scalameta:metals_2.12:%VERSION%" -r "bintray:scalacenter/releases" -r "sonatype:public" -r "sonatype:snapshots" -o metals
