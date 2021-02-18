71dc3cf0476a48c9d66e7b0c8d26dc57b3a16f44
Matthias Simon <matthias.simon@nokia.com>
2021-02-18 23:10:07 +0100

    Update ntt release URL

--- installer/install-ntt.cmd
+++ installer/install-ntt.cmd
@@ -2,4 +2,4 @@
 
 setlocal
 set VERSION=0.5
-curl -L "https://github.com/nokia/ntt/releases/download/v%VERSION%/ntt_windows_x86_64.tar.gz" | tar xz ntt.exe
+curl -L "https://github.com/nokia/ntt/releases/latest/download/ntt_windows_x86_64.tar.gz" | tar xz ntt.exe
