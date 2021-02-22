3f9f1036582cef167e1fbacfe1a50e3bf3667406
Prabir Shrestha <mail@prabir.me>
2021-02-22 11:28:32 +0000

    use latest tag directly

--- installer/install-rust-analyzer.sh
+++ installer/install-rust-analyzer.sh
@@ -13,13 +13,7 @@ darwin)
   ;;
 esac
 
-if [ ! which grep &> /dev/null ] || [ ! which cut &> /dev/null ]; then
-  version="2021-02-15"
-else
-  version=$(curl -sL https://api.github.com/repos/rust-analyzer/rust-analyzer/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
-fi
-
-curl -L -o rust-analyzer-$platform "https://github.com/rust-analyzer/rust-analyzer/releases/download/$version/rust-analyzer-$platform"
+curl -L -o "rust-analyzer-$platform" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$platform"
 
 mv rust-analyzer-$platform rust-analyzer
 chmod +x rust-analyzer
