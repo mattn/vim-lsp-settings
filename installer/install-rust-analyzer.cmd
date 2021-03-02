e61996cee838250ddd74ea11a11edae2315a08ae
Prabir Shrestha <mail@prabir.me>
2021-03-01 18:15:34 -0800

    use latest tag for windows

--- installer/install-rust-analyzer.cmd
+++ installer/install-rust-analyzer.cmd
@@ -1,7 +1,6 @@
 @echo off
 
 setlocal
-set VERSION=2021-02-22
-curl -L -o rust-analyzer-windows.exe "https://github.com/rust-analyzer/rust-analyzer/releases/download/%VERSION%/rust-analyzer-windows.exe"
+curl -L -o "rust-analyzer-windows.exe" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-windows.exe"
 
 move rust-analyzer-windows.exe rust-analyzer.exe
