e0269b5f672c3887c231f5223e5e12720e78a89e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-26 01:01:49 +0900

    Fix installer for julia-language-server

--- installer/install-julia-language-server.cmd
+++ installer/install-julia-language-server.cmd
@@ -5,19 +5,7 @@ julia -e "using Pkg; Pkg.add(PackageSpec(name=\"SymbolServer\", rev=\"master\"))
 julia -e "using Pkg; Pkg.add(PackageSpec(name=\"CSTParser\", rev=\"master\"))"
 
 echo ^
-import LanguageServer ^
-
-import Pkg ^
-
-import SymbolServer ^
-
-envpath = dirname(Pkg.Types.Context().env.project_file) ^
-
-server = LanguageServer.LanguageServerInstance(stdin, stdout, false, envpath) ^
-
-server.runlinter = true ^
-
-run(server) ^
+using LanguageServer, LanguageServer.SymbolServer; runserver() ^
 
 > languageserver.jl
 
