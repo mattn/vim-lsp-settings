8093c28743f32ae8f831828d3f0b683e7045374a
Prabir Shrestha <mail@prabir.me>
2021-05-28 06:55:26 +0000

    fix linux installer for rust analyzer

--- installer/install-rust-analyzer.sh
+++ installer/install-rust-analyzer.sh
@@ -6,14 +6,15 @@ os=$(uname -s | tr "[:upper:]" "[:lower:]")
 
 case $os in
 linux)
-  platform="linux"
+  platform="x86_64-unknown-linux-gnu"
   ;;
 darwin)
-  platform="mac"
+  platform="x86_64-apple-darwin"
   ;;
 esac
 
-curl -L -o "rust-analyzer-$platform" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$platform"
+curl -L -o "rust-analyzer-$platform.gz" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$platform.gz"
+gzip -d "rust-analyzer-$platform.gz"
 
 mv rust-analyzer-$platform rust-analyzer
 chmod +x rust-analyzer
