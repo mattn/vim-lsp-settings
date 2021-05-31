0f9feec183f64bf6f504bd122bad000878632552
Prabir Shrestha <mail@prabir.me>
2021-05-31 10:45:33 -0700

    use busybox for run_gzip if gzip not present

--- installer/run_gzip.cmd
+++ installer/run_gzip.cmd
@@ -4,6 +4,6 @@ where gzip 2>NUL
 if %ERRORLEVEL% equ 0 (
     gzip -d %*
 ) else (
-    curl -L -o %~dp0\gzip.exe https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/gzip.exe
-    %~dp0\gzip -d %*
+    curl -L -o %~dp0\busybox.exe https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/busybox.exe
+    %~dp0\busybox gzip -d %*
 )
