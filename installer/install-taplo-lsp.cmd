0cfb5381e28c106c795a1e52f5e93cdd1eefc948
kkiyama117 <k.kiyama117@gmail.com>
2021-09-29 06:37:11 +0900

    Fix download url of taplo-lsp for windows

--- installer/install-taplo-lsp.cmd
+++ installer/install-taplo-lsp.cmd
@@ -3,8 +3,7 @@
 setlocal
 
 set LATEST=0.2.6
-curl -L "https://github.com/tamasfe/taplo/releases/download/release-lsp-%LATEST%/taplo-lsp-$platform.tar.gz"
-
+curl -L "https://github.com/tamasfe/taplo/releases/download/release-lsp-%LATEST%/taplo-lsp-windows-x86_64.zip"
 call "%~dp0\run_unzip.cmd" taplo-lsp-windows-x86_64.zip
 del taplo-lsp-windows-x86_64.zip
 
