9ffa02c6ff70210bd34007d08eeb216173c430a4
kkiyama117 <k.kiyama117@gmail.com>
2021-09-29 05:57:46 +0900

    Add installer file

---
+++ installer/install-taplo-lsp.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+setlocal
+
+set LATEST=0.2.6
+curl -L "https://github.com/tamasfe/taplo/releases/download/release-lsp-%LATEST%/taplo-lsp-$platform.tar.gz"
+
+call "%~dp0\run_unzip.cmd" taplo-lsp-windows-x86_64.zip
+del taplo-lsp-windows-x86_64.zip
+
+%%~dp0\taplo-lsp.exe %%* ^
+
+> taplo-lsp.cmd
+
