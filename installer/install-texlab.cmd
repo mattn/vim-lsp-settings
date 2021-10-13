84858ca9356202fca4e5ee57345f276c74d9cb78
Dominique <dominique.orban@gmail.com>
2021-10-12 22:58:18 -0400

    install latest texlab on Windows

--- installer/install-texlab.cmd
+++ installer/install-texlab.cmd
@@ -1,7 +1,6 @@
 @echo off
 
 setlocal
-set VERSION=1.8.0
-curl -L -o texlab-x86_64-windows.zip "https://github.com/latex-lsp/texlab/releases/download/v%VERSION%/texlab-x86_64-windows.zip"
+curl -L -o texlab-x86_64-windows.zip "https://github.com/latex-lsp/texlab/releases/latest/download/texlab-x86_64-windows.zip"
 call "%~dp0\run_unzip.cmd" texlab-x86_64-windows.zip
 del texlab-x86_64-windows.zip
