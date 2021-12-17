6cddb782beb71a0a56ff7cdc97e46601a3df2534
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-12-18 01:55:06 +0900

    Use go install

--- installer/go_install.cmd
+++ installer/go_install.cmd
@@ -7,7 +7,7 @@ setlocal
 set GO111MODULE=on
 set GOPATH=%cd%
 set GOBIN=%cd%
-go get -v -u %1
+go install -v %1
 set GOBIN=
 go clean -modcache
 rd /S /Q "src" "pkg" 2> NUL
--- installer/go_install.sh
+++ installer/go_install.sh
@@ -5,6 +5,6 @@
 
 set -e
 
-GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v "$1"
+GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go install -v "$1"
 GOPATH=$(pwd) GO111MODULE=on go clean -modcache
 rm -rf src pkg 2>/dev/null
