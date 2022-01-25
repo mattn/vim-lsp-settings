20e6ed7b289a59b77c995ec6b378d6fe7402781e
ichizok <gclient.gaap@gmail.com>
2022-01-25 11:48:49 +0900

    Fix warnings about the deprecated options of bundle
    Use "bundle config set --local" to set "path" and "without" flags.

--- installer/install-solargraph.sh
+++ installer/install-solargraph.sh
@@ -3,7 +3,9 @@
 set -e
 
 git clone --depth=1 https://github.com/castwide/solargraph .
-bundle install --without development --path vendor/bundle
+bundle config set --local path vendor/bundle
+bundle config set --local without development
+bundle install
 
 cat <<EOF >solargraph
 #!/usr/bin/env bash
