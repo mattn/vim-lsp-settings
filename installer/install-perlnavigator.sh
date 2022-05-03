383982faf33def926f0640ef70ad2f9b12f0d078
Songmu <y.songmu@gmail.com>
2022-05-03 17:02:56 +0900

    fix perl installer

--- installer/install-perlnavigator.sh
+++ installer/install-perlnavigator.sh
@@ -17,6 +17,6 @@ esac
 
 curl -L -o perlnavigator-$os-x86_64.zip "https://github.com/bscan/PerlNavigator/releases/latest/download/perlnavigator-$os-x86_64.zip"
 unzip perlnavigator-$os-x86_64.zip
-mv perlnavigator-os-x86_64/perlnavigator .
-rm -rf perlnavigator-os-x86_64 perlnavigator-os-x86_64.zip
+mv perlnavigator-$os-x86_64/perlnavigator .
+rm -rf perlnavigator-$os-x86_64 perlnavigator-$os-x86_64.zip
 
