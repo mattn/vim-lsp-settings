e053161c689e6293c7074cf0df763981d0ae776a
mira <g1931032@m.sojo-u.ac.jp>
2022-05-06 23:16:50 +0900

    add curl output option

--- installer/install-taplo-lsp.cmd
+++ installer/install-taplo-lsp.cmd
@@ -3,7 +3,7 @@
 setlocal
 
 set LATEST=0.2.6
-curl -L "https://github.com/tamasfe/taplo/releases/download/release-lsp-%LATEST%/taplo-lsp-windows-x86_64.zip"
+curl -L "https://github.com/tamasfe/taplo/releases/download/release-lsp-%LATEST%/taplo-lsp-windows-x86_64.zip" -o taplo-lsp-windows-x86_64.zip
 call "%~dp0\run_unzip.cmd" taplo-lsp-windows-x86_64.zip
 del taplo-lsp-windows-x86_64.zip
 
