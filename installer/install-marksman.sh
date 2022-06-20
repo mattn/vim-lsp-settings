4b00cce39bee6f66cbb562b900c42ea5f969d2ff
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-20 22:18:42 +0900

    add marksman

---
+++ installer/install-marksman.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+os=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+case $os in
+linux)
+  platform="linux"
+  ;;
+darwin)
+  platform="macos"
+  ;;
+esac
+
+curl -L -o marksman "https://github.com/artempyanykh/marksman/releases/latest/download/marksman-$platform"
