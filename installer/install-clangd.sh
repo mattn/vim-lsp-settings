3ffdfc5b60a7e90878dc53b42e5040b3c904a6d9
miyukic <m.yukic10@gmail.com>
2022-07-01 15:23:06 +0000

    Fix Clangd installation failure on ubuntu 22.04

--- installer/install-clangd.sh
+++ installer/install-clangd.sh
@@ -58,6 +58,8 @@ filename() {
     14.04 | 16.04 | 18.04 | 20.04)
       local platform="linux-gnu-ubuntu-$ubuntu_version"
       ;;
+    22.04)
+      local platform="linux-gnu-ubuntu-20.04"
     esac
     ;;
   # Check LinuxMint version
