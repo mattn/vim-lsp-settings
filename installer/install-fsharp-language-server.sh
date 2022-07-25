0f6c9f40831476f66fc9d6cc5014fde6a3b77aad
Rune Ibsen <rune@runeibsen.dk>
2022-07-25 15:01:59 +0000

    Launch fsharp-language-server with dotnet
    On .NET, an assembly is not invoked on itself. Instead, we tell the
    dotnet runtime to launch the application.

--- installer/install-fsharp-language-server.sh
+++ installer/install-fsharp-language-server.sh
@@ -10,7 +10,7 @@ cat <<EOF >fsharp-language-server
 #!/bin/sh
 
 DIR=\$(cd \$(dirname \$0); pwd)
-\$DIR/Publish/FSharpLanguageServer.dll \$*
+dotnet \$DIR/Publish/FSharpLanguageServer.dll \$*
 EOF
 
 chmod +x fsharp-language-server
