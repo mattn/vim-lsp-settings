48fba30798e92e54ac2d0ea34ad3018915f8ddc6
Yuta Sakurai <sakurai.yuta@gmail.com>
2022-08-15 16:43:58 +0900

    fix: clojure-lsp installer for Windows.

--- installer/install-clojure-lsp.cmd
+++ installer/install-clojure-lsp.cmd
@@ -1,5 +1,22 @@
 @echo off
 
 setlocal
-set VERSION=2021.02.01-20.37.52
-curl -L -o clojure-lsp.cmd https://github.com/clojure-lsp/clojure-lsp/releases/download/%VERSION%/clojure-lsp
+
+set VERSION=latest
+
+rem FIXME: Windows shell always sets %PROCESSOR_ARCHITECTURE% ["AMD64" || "x86"] even if uses ARM64.
+echo Your Architecture is: %PROCESSOR_ARCHITECTURE%
+if %PROCESSOR_ARCHITECTURE% == AMD64 (
+  set ARCH=amd64
+) else (
+  echo Unknown architecture: %PROCESSOR_ARCHITECTURE%
+  exit /B 1
+)
+
+rem FIXME: clojure-lsp releases only AMD64 binary.
+set FILENAME=clojure-lsp-native-windows-%ARCH%.zip
+
+set URL=https://github.com/clojure-lsp/clojure-lsp/releases/%VERSION%/download/%FILENAME%
+echo Downloading: %URL%
+curl -L -O %URL%
+call "%~dp0\run_unzip.cmd" %FILENAME%
