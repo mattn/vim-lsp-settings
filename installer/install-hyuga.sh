23029f0b773e45093476ceb59f8755ca62b58462
Yuta Sakurai <sakurai.yuta@gmail.com>
2022-09-05 05:57:39 +0900

    fix hyuga installer(added --system-site-package option)

--- installer/install-hyuga.sh
+++ installer/install-hyuga.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-"$(dirname "$0")/pip_install.sh" hyuga hyuga
+#"$(dirname "$0")/pip_install.sh" hyuga hyuga
+
+# need to add --system-site-package option
+python3 -m venv --system-site-package ./venv
+./venv/bin/pip3 install -U pip
+./venv/bin/pip3 install ~/git/hyuga
+ln -s "./venv/bin/hyuga" .
