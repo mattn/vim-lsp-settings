2b489d4fe2617d8a7374e82eb76e4acf753ce060
tsukkee <takayuki0510@gmail.com>
2022-10-17 13:27:34 +0900

    update volar version
    Use "~" annotation because now volar  becomes stable and adopts semantic versioning.

--- installer/install-volar-server.cmd
+++ installer/install-volar-server.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-call "%~dp0\npm_install.cmd" vue-language-server @volar/vue-language-server@0.40.13
+call "%~dp0\npm_install.cmd" vue-language-server @volar/vue-language-server@~1.0.0
 ren vue-language-server.cmd volar-server.cmd
 call npm install typescript@4.8
