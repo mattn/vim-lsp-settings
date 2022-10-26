44b34b17000b6d62d133a3adf315d6205260167f
Morgan Larosa <chaos95@chaos95.com>
2022-10-26 14:18:29 +1100

    Fix Windows installation when username has space in it
    - Modify run_unzip.cmd to wrap invocations and paths in quotes
      - This prevents issues where a user's home directory has spaces in it due to their username, such as 'C:\Users\FirstName LastName', from causing commands to fail because they parse the different parts of the path as separate arguments.

--- installer/run_unzip.cmd
+++ installer/run_unzip.cmd
@@ -4,8 +4,8 @@ if "x%1" equ "x" goto :EOF
 
 where unzip 2>NUL
 if %ERRORLEVEL% equ 0 (
-    unzip %*
+    unzip "%*"
 ) else (
-    curl -L -o %~dp0\unzip.exe https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/unzip.exe
-    %~dp0\unzip %*
+    curl -L -o "%~dp0\unzip.exe" https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/unzip.exe
+    "%~dp0\unzip" "%*"
 )
