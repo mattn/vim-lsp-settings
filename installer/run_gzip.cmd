1c2fe64d42803bd0b9e05a6af450d54b58598c4e
Morgan Larosa <chaos95@chaos95.com>
2022-10-26 14:26:01 +1100

    Fix Windows installation space handling some more
    - This change applies the same fix to run_gzip.cmd as the previous
      commit.

--- installer/run_gzip.cmd
+++ installer/run_gzip.cmd
@@ -2,8 +2,8 @@
 
 where gzip 2>NUL
 if %ERRORLEVEL% equ 0 (
-    gzip -d %*
+    gzip -d "%*"
 ) else (
-    curl -L -o %~dp0\busybox.exe https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/busybox.exe
-    %~dp0\busybox gzip -d %*
+    curl -L -o "%~dp0\busybox.exe" https://github.com/mattn/vim-lsp-settings/releases/download/v0.0.1/busybox.exe
+    "%~dp0\busybox" gzip -d "%*"
 )
