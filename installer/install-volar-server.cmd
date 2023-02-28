13f48e6c31578fcb72fa610fdecacda4132db97f
tsukkee <takayuki0510@gmail.com>
2023-02-28 10:46:55 +0900

    update volar to v1.2.0

--- installer/install-volar-server.cmd
+++ installer/install-volar-server.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-call "%~dp0\npm_install.cmd" vue-language-server @volar/vue-language-server@~1.1.0
+call "%~dp0\npm_install.cmd" vue-language-server @volar/vue-language-server@~1.2.0
 ren vue-language-server.cmd volar-server.cmd
 call npm install typescript@4.8
