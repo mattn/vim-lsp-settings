9210f0a31e21083a9d00e84d057f98a5cb4e28ce
Daniel Viberg <daniel_viberg@outlook.com>
2023-04-28 09:14:30 +0200

    Updated ts in volar installer to solve auto import issue

--- installer/install-volar-server.cmd
+++ installer/install-volar-server.cmd
@@ -2,4 +2,4 @@
 
 call "%~dp0\npm_install.cmd" vue-language-server @volar/vue-language-server@~1.2.0
 ren vue-language-server.cmd volar-server.cmd
-call npm install typescript@4.8
+call npm install typescript@5.0.2
--- installer/install-volar-server.sh
+++ installer/install-volar-server.sh
@@ -4,4 +4,4 @@ set -e
 
 "$(dirname "$0")/npm_install.sh" vue-language-server @volar/vue-language-server@~1.2.0
 mv vue-language-server volar-server
-npm install typescript@4.8
+npm install typescript@5.0.2
