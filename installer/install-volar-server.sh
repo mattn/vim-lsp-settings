0d1be6f823f32256b141b02e7fe47f94dfad5fb2
tsukkee <takayuki0510@gmail.com>
2023-05-15 17:05:45 +0900

    update volar to v1.6.0

--- installer/install-volar-server.sh
+++ installer/install-volar-server.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-"$(dirname "$0")/npm_install.sh" vue-language-server @volar/vue-language-server@~1.4.0
+"$(dirname "$0")/npm_install.sh" vue-language-server @volar/vue-language-server@~1.6.0
 mv vue-language-server volar-server
 npm install typescript@5.0.2
