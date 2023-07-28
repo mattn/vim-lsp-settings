1738c3a30c1e87f656ad9aeeff84a051ff623700
pojiro <pojiro.jp@gmail.com>
2023-07-29 07:43:28 +0900

    Update elixir-ls version to 0.15.1

--- installer/install-elixir-ls.cmd
+++ installer/install-elixir-ls.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=0.14.6
+set VERSION=0.15.1
 curl -L -o elixir-ls.zip "https://github.com/elixir-lsp/elixir-ls/releases/download/v%VERSION%/elixir-ls.zip"
 call "%~dp0\run_unzip.cmd" elixir-ls.zip
 del elixir-ls.zip
