7613a3f702ae7ff2794b659a9769494203f5cb67
gawetto <5627164+gawetto@users.noreply.github.com>
2023-09-12 13:54:09 +0900

    tailwindcss-intelisense version 0.5.10 -> 0.10.0
    fix

--- installer/install-tailwindcss-intellisense.cmd
+++ installer/install-tailwindcss-intellisense.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=0.5.10
+set VERSION=0.10.0
 curl -L -o "vscode-tailwindcss.vsix" "https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v%VERSION%/vscode-tailwindcss-%VERSION%.vsix"
 
 call "%~dp0\run_unzip.cmd" vscode-tailwindcss.vsix
@@ -9,6 +9,6 @@ rem del vscode-tailwindcss.vsix
 
 echo @echo off ^
 
-node %%~dp0\extension\dist\server\index.js %%* ^
+node %%~dp0\extension\dist\tailwindServer.js %%* ^
 
 > tailwindcss-intellisense.cmd
