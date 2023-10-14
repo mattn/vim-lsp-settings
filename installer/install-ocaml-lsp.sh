7008bd5d7416944db6a10515d7e4601b3a56d610
msfukui <msfukui@gmail.com>
2023-10-14 23:17:02 +0900

    Fix build error for ocaml-lsp on macOS Sonoma

--- installer/install-ocaml-lsp.sh
+++ installer/install-ocaml-lsp.sh
@@ -4,23 +4,18 @@ set -e
 
 DEFAULT_DIR="$(pwd)"
 
-# We should not download GitHub's zip file here, because it doesn't include some submodules.
-git clone --recurse-submodules http://github.com/ocaml/ocaml-lsp.git ocaml-lsp-files --depth=1
+mkdir -p ocaml-lsp-files
 cd ocaml-lsp-files
 
-rm -r lsp/test
 OPAMROOT="$(pwd)/.opam"
 export OPAMROOT
 
 export OPAMYES=true
 opam init -a -n
-opam switch create . ocaml-base-compiler.4.14.0
+opam switch create . ocaml-base-compiler.4.14.1
 eval "$(opam env)" 2>/dev/null
-opam exec make install-test-deps
-opam exec make all
-
-rm -rf .git
+opam install ocaml-lsp-server
 
 cd "$DEFAULT_DIR"
-ln -snf "./ocaml-lsp-files/_build/default/ocaml-lsp-server/bin/main.exe" ocaml-lsp
+ln -snf "./ocaml-lsp-files/_opam/bin/ocamllsp" ocaml-lsp
 chmod +x ocaml-lsp
