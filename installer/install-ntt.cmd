aaf5634dccabca765a09e05dc9e086c107130178
moosq <53606936+moosq@users.noreply.github.com>
2023-10-23 15:39:35 +0200

    new file format for ntt archive for windows
    From `ntt v0.18.0` on the archive deployed for windows operating system will be in `. zip` format

--- installer/install-ntt.cmd
+++ installer/install-ntt.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 setlocal
-curl -LO "https://github.com/nokia/ntt/releases/latest/download/ntt_windows_x86_64.tar.gz"
-tar xvf ntt_windows_x86_64.tar.gz
-rm ntt_windows_x86_64.tar.gz
+curl -LO "https://github.com/nokia/ntt/releases/latest/download/ntt_windows_x86_64.zip"
+unzip ntt_windows_x86_64.zip
+rm ntt_windows_x86_64.zip
