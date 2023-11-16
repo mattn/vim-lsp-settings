a0c5cf830a45795142b65ff38268265889757a00
sgur <255021+sgur@users.noreply.github.com>
2023-11-16 11:00:15 +0900

    Update non-windows installer

--- installer/install-powershell-languageserver.sh
+++ installer/install-powershell-languageserver.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-curl -L -o PowerShellEditorServices.zip "https://github.com/PowerShell/PowerShellEditorServices/releases/download/v2.1.0/PowerShellEditorServices.zip"
+VERSION=3.14.0
+curl -L -o PowerShellEditorServices.zip "https://github.com/PowerShell/PowerShellEditorServices/releases/download/v$VERSION/PowerShellEditorServices.zip"
 unzip PowerShellEditorServices.zip
 rm PowerShellEditorServices.zip
 mkdir session
@@ -11,7 +12,7 @@ cat <<EOF >powershell-languageserver
 #!/bin/sh
 
 DIR=\$(cd \$(dirname \$0); pwd)
-PSES_BUNDLE_PATH=\$DIR/PowerShellEditorServices
+PSES_BUNDLE_PATH=\$DIR
 SESSION_TEMP_PATH=\$DIR/session
 pwsh -NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "\$PSES_BUNDLE_PATH/PowerShellEditorServices/Start-EditorServices.ps1 -BundledModulesPath \$PSES_BUNDLE_PATH -LogPath \$SESSION_TEMP_PATH/logs.log -SessionDetailsPath \$SESSION_TEMP_PATH/session.json -FeatureFlags @() -AdditionalModules @() -HostName 'My Client' -HostProfileId 'myclient' -HostVersion 1.0.0 -Stdio -LogLevel Normal"
 EOF
