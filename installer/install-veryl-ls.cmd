8e1ae69947743ba143e1c1c64b4e72a7956b4165
dalance <dalance@gmail.com>
2024-02-28 19:04:06 +0900

    Change URL of veryl-ls

--- installer/install-veryl-ls.cmd
+++ installer/install-veryl-ls.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-curl -L -o veryl-x86_64-windows.zip "https://github.com/dalance/veryl/releases/latest/download/veryl-x86_64-windows.zip"
+curl -L -o veryl-x86_64-windows.zip "https://github.com/veryl-lang/veryl/releases/latest/download/veryl-x86_64-windows.zip"
 call "%~dp0\run_unzip.cmd" veryl-x86_64-windows.zip
 del veryl.exe
 del veryl-x86_64-windows.zip
