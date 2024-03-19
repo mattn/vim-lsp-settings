133cc01b48fc2537a810f1e906cb8b50dc22f3f5
Robert <39488490+aiuno@users.noreply.github.com>
2024-03-19 13:16:37 +0000

    ols_ is a private repo, fixed it.

--- installer/install-ols.sh
+++ installer/install-ols.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-git clone --depth=1 https://github.com/DanielGavin/ols_
-cd ols_ && ./build.sh && cp ols .. && cd .. && rm -rf ols_
+git clone --depth=1 https://github.com/DanielGavin/ols
+cd ols && ./build.sh && cp ols .. && cd .. && rm -rf ols
