3f1ca4d30df865c52ed95e356683ed7c5dfadc30
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-07-01 18:08:17 +0900

    fix download path

--- installer/install-helm-ls.sh
+++ installer/install-helm-ls.sh
@@ -16,7 +16,7 @@ darwin)
   if [ "$(uname -m)" = "x86_64" ]; then
     filename="helm_ls_darwin_amd64"
   else
-    filename="helm_ls_darwin_arm"
+    filename="helm_ls_darwin_arm64"
   fi
   ;;
 esac
