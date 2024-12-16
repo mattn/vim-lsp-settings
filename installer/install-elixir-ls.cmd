ea45292c66e11b657a3c0b021f14efdb72bbccfc
Bernard Duggan <bernard@circl.es>
2024-12-16 11:17:38 +1100

    Update elixir-ls to 0.25.0

--- installer/install-elixir-ls.cmd
+++ installer/install-elixir-ls.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set VERSION=v0.23.0
+set VERSION=v0.25.0
 set ZIP=elixir-ls-%VERSION%.zip
 curl -L -o "%ZIP%" "https://github.com/elixir-lsp/elixir-ls/releases/download/%VERSION%/%ZIP%"
 call "%~dp0\run_unzip.cmd" "%ZIP%"
