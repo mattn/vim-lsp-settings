cfca2c64cceb8d0879c4a79b42eca0b5d5e6c6b6
ansanloms <ansanloms@users.noreply.github.com>
2025-01-15 02:14:35 +0900

    fix: Change rust-analyzer binary download URL for Windows.

--- installer/install-rust-analyzer.cmd
+++ installer/install-rust-analyzer.cmd
@@ -1,7 +1,6 @@
 @echo off
 
 setlocal
-curl -L -o "rust-analyzer-windows.gz" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-pc-windows-msvc.gz"
-call "%~dp0\run_gzip.cmd" rust-analyzer-windows.gz
-
-move rust-analyzer-windows rust-analyzer.exe
+curl -L -o "rust-analyzer-windows.zip" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-pc-windows-msvc.zip"
+call "%~dp0\run_unzip.cmd" rust-analyzer-windows.zip
+del rust-analyzer-windows.zip rust_analyzer.pdb
