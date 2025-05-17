b759e8894678c37c401c58c2034674521aa9c7a0
Yuta Sakurai <sakurai.yuta@gmail.com>
2025-05-18 03:16:27 +0900

    fennel-ls: added comments

--- installer/install-fennel-ls.sh
+++ installer/install-fennel-ls.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+# FIXME: official repository is not available for Windows
+# git clone https://git.sr.ht/~xerool/fennel-ls src
 git clone https://git.sr.ht/~y_sakurai/fennel-ls src
 cd src/
 make
