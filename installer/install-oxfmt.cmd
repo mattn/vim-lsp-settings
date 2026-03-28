09a552113037b0622711b5f1d354d0737cb24f15
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2026-03-28 12:28:43 +0900

    Fix install oxfmt from npm registry

--- installer/install-oxfmt.cmd
+++ installer/install-oxfmt.cmd
@@ -1,19 +1,3 @@
 @echo off
 
-setlocal
-
-set ARCH=%PROCESSOR_ARCHITECTURE%
-
-if /I "%ARCH%"=="ARM64" (
-  set ZIPNAME=oxfmt-aarch64-pc-windows-msvc.zip
-  set EXEFILE=oxfmt-aarch64-pc-windows-msvc.exe
-) else (
-  set ZIPNAME=oxfmt-x86_64-pc-windows-msvc.zip
-  set EXEFILE=oxfmt-x86_64-pc-windows-msvc.exe
-)
-
-curl -L -o "%ZIPNAME%" "https://github.com/oxc-project/oxc/releases/latest/download/%ZIPNAME%"
-call "%~dp0\run_unzip.cmd" "%ZIPNAME%"
-
-move "%EXEFILE%" oxfmt.exe
-del "%ZIPNAME%"
+call "%~dp0\npm_install.cmd" oxfmt oxfmt
